32e247c5e10ddae76615ac245daf6169c4b254ef
AKHILESH18100 <akhilesh18100@gmail.com>
2025-05-11 14:42:58 +0530

    Add files via upload

---
+++ statickey.java
@@ -0,0 +1,22 @@
+public class statickey{
+public static void main(String[] args) {
+    School s1= new School();
+    School s2= new School();
+    School s3= new School();
+    s2.name="RSS";
+    s3.name="BRS";
+    s3.setname();
+    s2.setname();
+
+    
+}
+}
+class School{
+       String name;
+    void setname(){
+        System.out.println(this.name);
+    } 
+    void getname(){
+        System.out.println();
+    }
+}
